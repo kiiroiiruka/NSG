d75dcecfbd7520f696fc1a6ed11d0471081e56d0
tmkttmkt <24rd123@ms.dendai.ac.jp>
2024-10-02 13:42:01 +0900

    xom

---
+++ back/api/main.js
@@ -0,0 +1,24 @@
+//import
+const express = require('express');
+const cors = require('cors');
+//本体
+const app = express();
+const PORT =5000;
+
+//ハードより。いろいろアクセスできるようにする
+app.use(cors());
+app.use(express.json());
+
+
+//http//の画面
+app.get('/', (req, res) => {
+  console.log("helloword");
+  res.json({t:"helloword"})
+  });
+app.use((req, res,next) => {
+  res.status(404).json({error:true,msg:'ドドドォ404ドン!!そんなページはない!!\n多分'});
+});
+
+
+//実行
+app.listen(PORT, () => console.log(`Server running at http://127.0.0.1:${PORT}/`),);
