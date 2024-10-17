f08337e5e711a36f46d968f45c7dfed9e0e8fb4f
tmkttmkt <24rd123@ms.dendai.ac.jp>
2024-10-17 13:51:52 +0900

    どこまでやったんだっけ？

---
+++ front/src/sava/testsend.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var admin = require("firebase-admin");
+var serviceAccount = require("./path/to/firebase-adminsdk.json");
+// Firebase Admin SDKを初期化
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+var db = admin.firestore();
+// 保存するJSONデータ
+var jsonData = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+// コレクションとドキュメントへの参照
+var docRef = db.collection('users').doc('user1');
+// JSONデータを保存
+docRef.set(jsonData).then(function () {
+    console.log('データ保存に成功しました!');
+}).catch(function (error) {
+    console.error('データ保存に失敗しました:', error);
+});
